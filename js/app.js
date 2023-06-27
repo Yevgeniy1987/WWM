a35a91a64d024a5ae0fcbfd4a6d267199cf2e8c2
Eugene <evgeniy.lyashko.87@gmail.com>
2023-06-26 22:02:56 -0500

    fix mistakes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 console.log("Hello");
-const movieElem = document.getElementById("movie-list");
+const movieList = document.getElementById("movie-list");
 const searchForm = document.getElementById("search-form");
 const addNewMovieBtn = document.getElementById("add-new-movie-btn");
 
@@ -17,17 +17,17 @@ fetch(`${URL_BASE}/movies`)
   .then((res) => res.json())
   .then((data) => {
     MOVIES = data;
-    renderMovieList(movieElem, MOVIES, true);
+    renderMovieList(movieList, MOVIES, true);
     console.log(MOVIES);
   });
 
 addNewMovieBtn.addEventListener("click", showNewMovieModal);
 
-movieElem.addEventListener("click", (event) => {
+movieList.addEventListener("click", (event) => {
   if (event.target.classList.contains("btn")) {
-    // const updateBtn = event.target;
-    // const updatingMovieId = Number(updateBtn.dataset.id);
-    showUpdatedMovieModal();
+    const updateBtn = event.target;
+    const updatingMovieId = Number(updateBtn.dataset.id);
+    showUpdatedMovieModal(updatingMovieId);
   }
 });
 
@@ -36,15 +36,16 @@ modalBody.addEventListener("submit", (event) => {
   const form = event.target;
   const formAction = form.dataset.action;
   if (formAction === "create") {
-    addMovie();
+    addMovie(form);
   }
-  // // if (formAction === "updateMovie") {
-  // //   updateMovie();
-  // }
+  if (formAction === "updateMovie") {
+    const updatingMovieId = Number(form.dataset.updatingmovieid);
+    updateMovie(form, updatingMovieId);
+  }
 });
 
 function showNewMovieModal() {
-  const modalForm = `<form data-action= "create" id="modalForm">
+  const modalForm = `<form data-action="create" id="modalForm">
       <div class="flex-col justify-between">
         <div class="modal-window-form-field">
           <label for="title">Name</label>
@@ -87,44 +88,44 @@ function showNewMovieModal() {
 
   openModal();
 }
-function showUpdatedMovieModal() {
-  const modalForm = `<form data-action= "updateMovie" id="modalForm">
+function showUpdatedMovieModal(updatingMovieId) {
+  const modalForm = `<form data-action="updateMovie" data-updatingMovieId="${updatingMovieId}" id="modalForm">
       <div class="flex-col justify-between">
         <div class="modal-window-form-field">
-          <label for ="title">Name</label>
-          <input name ="title" id = "title" class="inputField" type="text" placeholder="Enter name" />
+          <label for="title">Name</label>
+          <input name="title" id="title" class="inputField" type="text" placeholder="Enter name" />
         </div>
         <div class="modal-window-form-field">
           <label for="country">Country</label>
-          <input name="country" id = "country" class="inputField" type="text" placeholder="Enter country" />
+          <input name="country" id="country" class="inputField" type="text" placeholder="Enter country" />
         </div>
         <div class="modal-window-form-field">
           <label for="year">Year</label>
-          <input name="year" id = "year" class="inputField" type="text" placeholder="Enter year" />
+          <input name="year" id="year" class="inputField" type="text" placeholder="Enter year" />
         </div>
         <div class="modal-window-form-field">
           <label for="genre">Genre</label>
-          <input name="genre" id = "genre" class="inputField" type="text" placeholder="Enter genre" />
+          <input name="genre" id="genre" class="inputField" type="text" placeholder="Enter genre" />
         </div>
         <div class="modal-window-form-field">
           <label for="producer">Producer</label>
-          <input name="producer" id = "producer" class="inputField" type="text" placeholder="Enter producer's name" />
+          <input name="producer" id="producer" class="inputField" type="text" placeholder="Enter producer's name" />
         </div>
         <div class="modal-window-form-field">
           <label for="mainActor">Main Actor</label>
-          <input name="mainActor" id = "mainActor" class="inputField" type="text" placeholder="Enter Main Actor's name" />
+          <input name="mainActor" id="mainActor" class="inputField" type="text" placeholder="Enter Main Actor's name" />
         </div>
         <div class="modal-window-form-field">
           <label for="description">Description</label>
-          <input name="description" id = "description" class="inputField" type="text-area" placeholder="Enter Description" />
+          <input name="description" id="description" class="inputField" type="text-area" placeholder="Enter Description" />
         </div>
   
         <div>
-  <button class="h-7 w-28 border bg-yellow-400 rounded" type="submit" id="updateBtn">
-    Update
-  </button>
-  <button class="h-7 w-28 border bg-red-400 rounded" type="reset" id="resetBtn">Clear</button>
-</div>
+         <button class="h-7 w-28 border bg-yellow-400 rounded" type="submit">
+       Update
+        </button>
+        <button class="h-7 w-28 border bg-red-400 rounded" type="reset" >Clear</button>
+      </div>
 
       </div>
     </form>`;
@@ -135,7 +136,7 @@ function showUpdatedMovieModal() {
 
 function addMovie(form) {
   const title = form.title.value;
-  console.log(title)
+  console.log(title);
   const country = form.country.value;
   const year = form.year.value;
   const genre = form.genre.value;
@@ -167,7 +168,7 @@ function addMovie(form) {
       form.reset();
     });
 }
-function updateMovie(form) {
+function updateMovie(form, updatingMovieId) {
   const title = form.title.value;
   const country = form.country.value;
   const year = form.year.value;
@@ -185,34 +186,24 @@ function updateMovie(form) {
     description,
     updatedAt: new Date().toISOString(),
   };
-  
-  const updateMoveBtn = document.getElementById("updateBtn");
-  updateMoveBtn.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const actionBtn = event.target;
-    const updateBtn = actionBtn.dataset.action;
-     const currentUpdateBtn = event.target;
-    const updatingMovieId = Number(currentUpdateBtn.dataset.id);
-   
 
-    if (updateBtn === "update") {
-      fetch(`${URL_BASE}/movies/${updatingMovieId}`, {
-        method: "PATCH",
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-        body: JSON.stringify(updatedMovieCard),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          MOVIES[updatingMovieId] = data;
-          renderMovieList(movieElem, MOVIES, true);
-        });
-    }
-  });
+  const updatingMovingIdx = MOVIES.findIndex(
+    (movie) => movie.id === updatingMovieId
+  );
 
-  
+  fetch(`${URL_BASE}/movies/${updatingMovieId}`, {
+    method: "PATCH",
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+    body: JSON.stringify(updatedMovieCard),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      MOVIES[updatingMovingIdx] = data;
+      renderMovieList(movieList, MOVIES, true);
+      closeModal();
+    });
 }
+
 closeModalWindow.addEventListener("mousedown", (event) => {
   if (event.target === modalBackDrop || closeModalWindow) {
     closeModal();
